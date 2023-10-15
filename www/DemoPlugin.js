525055c63476395261cc21be1c6d8407edeb1369
JSTop1Coder <jstopcoder0209@gmail.com>
2023-10-16 05:24:03 +1000

    update

--- www/DemoPlugin.js
+++ www/DemoPlugin.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'DemoPlugin', 'coolMethod', [arg0]);
 };
+
+exports.initializeConnection = function(arg0, success, error) {
+    exec(success, error, 'DemoPlugin', 'initializeConnection', [arg0]);
+}
